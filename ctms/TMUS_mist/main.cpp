79fba774afb845482de0c240273d93e36ac9a6b8
mist <mist>
2005-03-12 17:08:06 +0000

    BUILD for Linux

---
+++ ctms/TMUS_mist/main.cpp
@@ -0,0 +1,25 @@
+
+#include <qapplication.h>
+#include <qtranslator.h>
+#include "ctms_upd_db_tool.h"
+#include <login.h>
+#include <qdialog.h>
+int main( int argc, char ** argv ) {
+    QApplication app( argc, argv );
+    QTranslator translator(0);
+
+    if (!translator.load("german","."))
+    	qDebug("Übersetzung hat nicht geklappt");
+    else
+    	qDebug("Die Übersetzung hat funktioniert");
+
+    app.installTranslator(&translator);
+
+    LoginDialog* UserReg = new LoginDialog();
+    UserReg->show();
+    ctms_upd_db_tool * mw = new ctms_upd_db_tool();
+    mw->setCaption("ctms_upd_db_tool" );
+    mw->show();
+    app.connect( &app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()) );
+    return app.exec();
+}
